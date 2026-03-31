2d3e857e4066433d9b9919ff1a430fc644602a18
Aditya Sudake <128912525+adityasudake108@users.noreply.github.com>
2026-03-31 09:15:21 +0530

    Create main Spring Boot application class

---
+++ src/main/java/com/example/cogniproject/CogniprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.cogniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogniprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogniprojectApplication.class, args);
+    }
+}
